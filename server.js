53be4374aaa95ce5642da31fe2636e951c86007d
BrandonLenz <lenzb@oregonstate.edu>
2022-10-29 00:33:38 -0400

    basic search functionality added

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
 const bodyParser = require("body-parser");
@@ -18,9 +17,43 @@ const { MongoClient } = require("mongodb");
 const uri = "mongodb+srv://brandonlenz:15XWJbReF3eXV0KY@cluster0.g2wvu5k.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
-app.get('/', async (req, res) => {
-    const out = await client.db('test');
-    console.log(out);
-    res.send('hi')
+app.post('/patients', async (req, res) => {
+  const lastName = req.body.lastName;
+  const firstName = req.body.firstName;
+  const dateOfBirth = req.body.dateOfBirth;
+  const memberId = req.body.memberId;
 
+  if (lastName === undefined && firstName === undefined && dateOfBirth === undefined && memberId === undefined) {
+    res.status(400);
+    return res.send({'Error' : 'Please provide last name, first name, date of birth, or the member ID'});
+  }
+  const patients = await client.db('Mini-EMR').collection('Patients').find().toArray();
+  const searchResults = [];
+
+  for (let i=0; i < patients.length; i++) {
+    const thisPatient = patients[i];
+    let [lastNameMatch, firstNameMatch, dateOfBirthMatch, memberIdMatch] = [false, false, false, false];
+
+    if (lastName !== undefined) {
+      lastNameMatch = thisPatient['LastName'] === lastName;
+    }
+
+    if (firstName !== undefined) {
+      firstNameMatch = thisPatient['FirstName'] === firstName;
+    }
+
+    if (dateOfBirth !== undefined) {
+      dateOfBirthMatch = thisPatient['DOB'] === dateOfBirth;
+    }
+
+    if (memberId !== undefined) {
+      memberIdMatch = thisPatient['MemberID'] === memberId;
+    }
+
+    if (lastNameMatch || firstNameMatch || dateOfBirthMatch || memberIdMatch) {
+      searchResults.push(thisPatient);
+    }
+  };
+
+  return res.send(searchResults);
 });
